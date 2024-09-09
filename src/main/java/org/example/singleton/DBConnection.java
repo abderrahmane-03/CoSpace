fab84886a4436e2bb09d82ec15f0591f927b248a
abderrahmane-03 <abdrahmanhafidi03@gmail.com>
2024-09-09 11:42:10 +0100

    DbConnection singleton

---
+++ src/main/java/org/example/singleton/DBConnection.java
@@ -0,0 +1,36 @@
+package org.example.singleton;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DBConnection {
+
+    private static final Logger LOGGER =
+            Logger.getLogger(DBConnection.class.getName());
+    private static Optional<Connection> connection = Optional.empty();
+
+    // Load environment variables
+    private static Dotenv dotenv = Dotenv.load();
+
+    public static Optional<Connection> getConnection() {
+        if (connection.isEmpty()) {
+            String url = dotenv.get("DB_URL");
+            String user = dotenv.get("DB_USER");
+            String password = dotenv.get("DB_PASSWORD");
+
+            try {
+                connection = Optional.ofNullable(
+                        DriverManager.getConnection(url, user, password));
+            } catch (SQLException ex) {
+                LOGGER.log(Level.SEVERE, null, ex);
+            }
+        }
+
+        return connection;
+    }
+}
